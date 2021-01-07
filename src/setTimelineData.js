5f41e3b3b7ad07b7e33899e26ef6119007bfeb56
javi_mac <javimayorque@gmail.com>
2021-01-07 15:39:41 -0600

    get date limits

--- src/setTimelineData.js
+++ src/setTimelineData.js
@@ -1,11 +1,22 @@
 import { monthNames } from './constants'
 import filterDocSet from './filterDocSet'
 
-// function getDateLimits (docs) {
-//     // returns the max and min dates
-//     let max, min
+function getDateLimits(docs) {
+    let min = null,
+        max = null
 
-// }
+    // console.log( 'docs:', docs )
+    docs.forEach(({ date }) => {
+        let docDate = new Date(date + 'T00:00:00')
+        if (!min || docDate < min) min = docDate
+        if (!max || docDate > max) max = docDate
+    })
+
+    return { min, max }
+}
+
+var dateLimits = getDateLimits(docs)
+console.log('max-min:', dateLimits)
 
 const timeLineData = monthNames.map(
     month =>
@@ -14,6 +25,14 @@ const timeLineData = monthNames.map(
             weeks: new Array(4).fill(null).map(_ => []),
         })
 )
+
+// const timeLineData = new Array()
+
+// const minYear = dateLimits.min.get
+// while(true) {
+//     if()
+// }
+
 
 timeLineData.forEach((month, monthIndex) =>
     month.weeks.forEach((week, weekIndex) =>
